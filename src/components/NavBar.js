26381fa66faf377370a80e13cb18ecba279f715d
kshyun1223 <kshyun.dev@gmail.com>
2023-02-19 19:32:02 +0900

    css 모듈

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import Link from "next/link"
 import { useRouter } from "next/router"
+import styles from "./NavBar.module.css"
 
 export default function NavBar() {
   const router = useRouter()
   return (
-    <nav>
+    <nav className={ styles.nav }>
       <Link href="/" style={{ color: router.pathname === "/" ? "red" : "blue"}}>Home</Link>
       <> </>
       <Link href="/about-next" style={{ color: router.pathname === "/about-next" ? "red" : "blue" }}>About</Link>
