06ae1434226db092d6ed8c4ac501dcc687dba860
kshyun1223 <kshyun.dev@gmail.com>
2023-02-19 19:26:27 +0900

    라우터에 접근하기 위한 router 모듈

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 export default function NavBar() {
-  return(
+  const router = useRouter()
+  return (
     <nav>
-      <Link href="/">Home</Link>
+      <Link href="/" style={{ color: router.pathname === "/" ? "red" : "blue"}}>Home</Link>
       <> </>
-      <Link href="/about-next">About</Link>
+      <Link href="/about-next" style={{ color: router.pathname === "/about-next" ? "red" : "blue" }}>About</Link>
     </nav>
   )
 }
