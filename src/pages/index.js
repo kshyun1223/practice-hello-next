18faeb7947670df9203145cd451b156f5597fb38
kshyun1223 <kshyun.dev@gmail.com>
2023-02-20 16:32:26 +0900

    style 태그에 global  prop을 부여하면 해당 페이지 안에서만 효과가 있다

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,11 @@ export default function Home() {
     <div>
       <NavBar />
       <h1>hello next!!!</h1>
+      <style jsx global>{`
+        a{
+          color:white;
+        }
+      `}</style>
     </div>
   )
 }
