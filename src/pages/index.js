1a5ad63a74a76bcae0c109edde92349aa246ab4f
kshyun1223 <kshyun.dev@gmail.com>
2023-02-21 18:10:37 +0900

    css 적용

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
@@ -19,12 +20,36 @@ export default function Home() {
     <div>
       <Seo title="Index" />
       <h1>Hello Next!!!</h1> 
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
-        <div key={movie.id}>
-          <h4>{movie.original_title}</h4>
-        </div>
-      ))}
+      <div className="container">
+        {!movies && <h4>Loading...</h4>}
+        {movies?.map((movie) => (
+          <div className="movie" key={movie.id}>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="img" />
+            <h4>{movie.original_title}</h4>
+          </div>
+        ))}
+        <style jsx>{`
+          .container {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            padding: 20px;
+            gap: 20px;
+          }
+          .movie img {
+            max-width: 100%;
+            border-radius: 12px;
+            transition: transform 0.2s ease-in-out;
+            box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+          }
+          .movie:hover img {
+            transform: scale(1.05) translateY(-10px);
+          }
+          .movie h4 {
+            font-size: 18px;
+            text-align: center;
+          }
+        `}</style>
+      </div>
     </div>
   )
 }
