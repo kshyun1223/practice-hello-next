5d83733e86df6edaff2cb5980ba3a840103c7478
kshyun1223 <kshyun.dev@gmail.com>
2023-02-21 17:44:35 +0900

    외부 api fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "249aafa794cc04e94eef1cf2be0a7589"
+
 export default function Home() {
+  const [movies, setMovies] = useState()
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json()
+      setMovies(results)
+    })()
+  }, [])
   return (
     <div>
       <Seo title="Index" />
       <h1>Hello Next!!!</h1> 
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   )
 }
